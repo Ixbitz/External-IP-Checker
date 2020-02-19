ef37e4f10f7eb4d7efe96ec47d275184c856db26
Job <me@ixbitz.com>
2020-02-19 13:54:26 +0100

    Fixed possible resource leak + general code optimalizations

--- src/main/java/com/ixbitz/utils/classes/utils/Utils.java
+++ src/main/java/com/ixbitz/utils/classes/utils/Utils.java
@@ -9,8 +9,6 @@ package com.ixbitz.utils.classes.utils;
 
 import com.ixbitz.utils.classes.ResponsePair;
 import com.ixbitz.utils.exceptions.HttpException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -35,15 +33,11 @@ public class Utils {
             httpURLConnection.setRequestMethod("GET");
             httpURLConnection.setInstanceFollowRedirects(true);
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-            String inputLine;
             StringBuilder content = new StringBuilder();
-
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
+            try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()))) {
+                bufferedReader.lines().forEach(content::append);
             }
 
-            in.close();
             httpURLConnection.disconnect();
 
             return new ResponsePair(httpURLConnection.getResponseCode(), content.toString());
